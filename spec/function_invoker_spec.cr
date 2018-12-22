c06b7e9e4c10abf2eefaf4095253c268ad3df841
TPei <t.s.peikert@gmail.com>
2018-12-22 18:48:28 +0100

    add basic specs for web requests being done

--- spec/function_invoker_spec.cr
+++ spec/function_invoker_spec.cr
@@ -1,9 +1,47 @@
 require "./spec_helper"
+require "webmock"
+
 
 describe FunctionInvoker do
-  # TODO: Write tests
+  describe "invoke" do
+    context "with a POST request" do
+      it "makes a HTTP call with the given data" do
+        WebMock.stub(:post, "gateway:8080/function/some_function").
+          with(body: "body").
+          to_return(status: 200, body: "response")
 
-  it "works" do
-    false.should eq(true)
+        resp = FunctionInvoker::Function.new("some_function").invoke("body")
+        resp.should eq JSON::Any.new("response")
+      end
+    end
+
+    context "with a GET request" do
+      it "makes a HTTP call with the given data" do
+        WebMock.stub(:get, "gateway:8080/function/some_function").
+          with(body: "some body").
+          to_return(status: 200, body: "response")
+
+        resp = FunctionInvoker::Function.new("some_function", "GET").invoke(
+          "some body"
+        )
+        resp.should eq JSON::Any.new("response")
+      end
+    end
+
+    context "with an error" do
+      it "raises a FunctionException" do
+        expect_raises FunctionInvoker::FunctionException do
+          FunctionInvoker::Function.new("some test").invoke
+        end
+      end
+
+      context "with ignore_errors: true" do
+        it "can ignore errors" do
+          resp = FunctionInvoker::Function.new("some test").
+            invoke(ignore_errors: true)
+          resp.should eq JSON::Any.new("")
+        end
+      end
+    end
   end
 end
